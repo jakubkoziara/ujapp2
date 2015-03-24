8a6d494c9cadf14cbae491c10f38c33ae974d7b2
Darek Finster <darek.finster@gmail.com>
2015-03-24 17:15:02 +0000

    Add Ability and devise implementation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  
+  load_and_authorize_resource
+
   def index
     @articles = Article.all
   end
---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, Article
+      can :manage, Comment
+    else
+      can :read, :all
+    end
+  end
+end
